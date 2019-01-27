7f7eb07d12fc7369f711b2e10a151425ec1c425f
XDD_6453 <511599737@qq.com>
2019-01-27 14:22:10 +0800

    add handi

--- GGJ/Assets/Scripts/BoyControl.cs
+++ GGJ/Assets/Scripts/BoyControl.cs
@@ -22,7 +22,18 @@ public class BoyControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        print("islooking  " +Mother.islooking);
+        if (Mother.islooking)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
         if (GameOver.isGameStart) {
+          
+
             currentTime += Time.deltaTime;
             if (currentTime > HairNewTime)
             {
